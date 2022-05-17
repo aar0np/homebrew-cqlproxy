0e547215c9e81d49101f96058dd61fdd38aa4ead
Aaron Ploetz <aaron.ploetz@datastax.com>
2022-05-17 10:03:25 -0500

    correcting SHA256

--- cql-proxy.rb
+++ cql-proxy.rb
@@ -1,8 +1,8 @@
 class CqlProxy < Formula
-  desc "The cql-proxy sidecar enables applications that are currently using Apache Cassandra or DataStax Enterprise (DSE) to use Astra DB without requiring any code changes."
+  desc "DataStax cql-proxy enables Cassandra apps to use Astra DB without code changes"
   homepage "https://github.com/datastax/cql-proxy"
   url "https://github.com/datastax/cql-proxy/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "060185758739b7bfcb719dd2d1679b35c7f8161866f110078a4395df9c2abe65"
+  sha256 "25cd9a87078c11caed7da14d2b8b85e5240bfe1aff5b666d0dcad9e73c15b305"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -12,6 +12,7 @@ class CqlProxy < Formula
   end
 
   test do
-    assert_match true, strings.HasPrefix(shell_output("./cql-proxy"), "cql-proxy: error: must provide either bundle path or contact points")
+    assert_match true, strings.HasPrefix(shell_output("./cql-proxy"),
+      "cql-proxy: error: must provide either bundle path or contact points")
   end
 end
